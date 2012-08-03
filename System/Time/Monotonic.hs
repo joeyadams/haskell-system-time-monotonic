ff47f058af80be2f782b186080c38191f6268144
Joey Adams <joeyadams3.14159@gmail.com>
2012-08-03 13:38:13 -0400

    Initial commit: System.Time.Monotonic, System.Time.Monotonic.Direct, and cabal init

---
+++ System/Time/Monotonic.hs
@@ -0,0 +1,86 @@
+-- |
+-- Module:      System.Time.Monotonic
+-- Copyright:   (c) Joseph Adams 2012
+-- License:     BSD3
+-- Maintainer:  joeyadams3.14159@gmail.com
+-- Portability: Tested on Linux and Windows.  Requires FFI, threads, and weak pointers.
+--
+-- Simple library for using the system's monotonic clock.  This library is
+-- intended to be portable and reliable, but does not (necessarily) provide
+-- high-resolution timing.
+--
+-- On Windows, this uses @GetTickCount@, and on Linux, it uses @clock_gettime@
+-- with @CLOCK_MONOTONIC@.  In both cases, the 'Clock' object avoids wraparound
+-- issues by getting the system time periodically, to avoid comparing two
+-- system times that are too far apart.
+module System.Time.Monotonic (
+    -- * Clock
+    Clock,
+    newClock,
+    clockGetTime,
+
+    -- * Time representation
+    MSec(..),
+) where
+
+import System.Time.Monotonic.Direct (getMonotonicTimeMSec32)
+
+import Control.Concurrent
+import Data.Int             (Int64)
+import Data.IORef
+import Data.Word            (Word32)
+import System.Mem.Weak      (deRefWeak)
+
+-- | A duration, measured in milliseconds.
+newtype MSec = MSec Int64
+    deriving (Eq, Ord, Show)
+
+newtype Clock = Clock (IORef ClockData)
+    deriving Eq
+
+-- | The externally reported time, paired with the return value of
+-- 'getMonotonicTimeMSec32', at a given point in time.
+data ClockData = ClockData !Int64 !Word32
+
+-- | Create a new 'Clock'.
+--
+-- Performance consideration: this will fork a thread that periodically gets
+-- the time and updates the clock's internal disposition.  That thread will go
+-- away when the 'Clock' is garbage collected.
+newClock :: IO Clock
+newClock = do
+    st <- getMonotonicTimeMSec32
+    ref <- newIORef (ClockData 0 st)
+    nudgePeriodically (Clock ref)
+    return (Clock ref)
+
+-- | Return the amount of time that has elapsed since the clock was created
+-- with 'newClock'.
+clockGetTime :: Clock -> IO MSec
+clockGetTime (Clock ref) = do
+    st2 <- getMonotonicTimeMSec32
+    t2 <- atomicModifyIORef ref $
+        \(ClockData t1 st1) ->
+            let t2 = t1 + fromIntegral (st2 - st1)
+             in (ClockData t2 st2, t2)
+    t2 `seq` return (MSec t2)
+
+nudgePeriodically :: Clock -> IO ()
+nudgePeriodically (Clock ref) = do
+    weak_mv <- newEmptyMVar
+
+    tid <- forkIO $ do
+        weak <- takeMVar weak_mv
+        let loop = do
+                m <- deRefWeak weak
+                case m of
+                    Nothing   -> return ()
+                    Just ref' -> do
+                        putStrLn "Nudge"
+                        _ <- clockGetTime (Clock ref')
+                        threadDelay 60000000
+                        loop
+         in loop
+
+    mkWeakIORef ref (killThread tid)
+        >>= putMVar weak_mv
---
+++ System/Time/Monotonic/Direct.hsc
@@ -0,0 +1,85 @@
+-- |
+-- Module:      System.Time.Monotonic.Direct
+-- Copyright:   (c) Joseph Adams 2012
+-- License:     BSD3
+-- Maintainer:  joeyadams3.14159@gmail.com
+-- Portability: Requires FFI; tested on Linux and Windows
+--
+-- This module provides more direct access to the system's monotonic clock, but
+-- does not take care of the 49.7 day wraparound issue for you.  Use with care.
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+module System.Time.Monotonic.Direct (
+    getMonotonicTimeMSec32,
+) where
+
+import Data.Word
+
+#if mingw32_HOST_OS
+#include <Windows.h>
+#else
+import Foreign
+import Foreign.C
+#include <time.h>
+#endif
+
+-- | Get the time from the system's monotonic clock, in milliseconds.  Due to
+-- the use of 'Word32', this has a wraparound of about 49.7 days.  However,
+-- this is not a problem, provided that you:
+--
+--  * Don't compare two results of 'getMonotonicTimeMSec32' that are more than 49.7
+--    days apart.
+--
+--  * Perform the subtraction on 'Word32' times.  Do this:
+--
+--    >now <- getMonotonicTimeMSec32
+--    >let elapsed = fromIntegral (now - before)
+--
+--    Not this:
+--
+--    >now <- getMonotonicTimeMSec32
+--    >let elapsed = fromIntegral now - fromIntegral before -- WRONG
+--
+--    When overflow occurs, subtraction modulo 2^32 will do the right thing.
+--    See <http://stackoverflow.com/a/9232314/149391>.
+--
+-- On Windows, this is just @GetTickCount@.  For systems that provide
+-- additional precision or have a larger wraparound, that information is simply
+-- truncated out.  This keeps overflow handling simple and consistent.
+getMonotonicTimeMSec32 :: IO Word32
+
+#if mingw32_HOST_OS
+
+getMonotonicTimeMSec32 = c_GetTickCount
+
+foreign import stdcall "Windows.h GetTickCount"
+    c_GetTickCount :: IO #{type DWORD}
+
+#else
+
+getMonotonicTimeMSec32 =
+    allocaBytes #{size struct timespec} $ \ptr -> do
+        throwErrnoIfMinus1_ "clock_gettime" $
+            c_clock_gettime #{const CLOCK_MONOTONIC} ptr
+        t <- peekCTimeSpec ptr
+        return $! fromIntegral (tv_sec t) * 1000 + fromIntegral (tv_nsec t `div` 1000000)
+
+foreign import ccall "time.h clock_gettime"
+    c_clock_gettime :: #{type clockid_t}
+                    -> Ptr CTimeSpec
+                    -> IO CInt
+
+data CTimeSpec = CTimeSpec
+    { tv_sec    :: !(#{type time_t})
+    , tv_nsec   :: !CLong
+    }
+
+peekCTimeSpec :: Ptr CTimeSpec -> IO CTimeSpec
+peekCTimeSpec ptr = do
+    sec  <- #{peek struct timespec, tv_sec}  ptr
+    nsec <- #{peek struct timespec, tv_nsec} ptr
+    return CTimeSpec { tv_sec  = sec
+                     , tv_nsec = nsec
+                     }
+
+#endif
