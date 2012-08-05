d1f3afb8e4904ed4035b5c8a0119aabc1ae65756
Joey Adams <joeyadams3.14159@gmail.com>
2012-08-05 16:30:30 -0400

    testing/clock.hs: Print the name of the clock driver

--- testing/clock.hs
+++ testing/clock.hs
@@ -1,9 +1,14 @@
 import System.Time.Monotonic
+import System.Time.Monotonic.Direct
 import Control.Monad (forever)
 
 main :: IO ()
 main = do
-    clock <- newClock
+    sclock <- getSystemClock
+    case sclock of
+        SomeSystemClock sc ->
+            putStrLn $ "Using " ++ systemClockName sc
+    clock <- newClockWithDriver sclock
     forever $ do
         print =<< clockGetTime clock
         _ <- getLine
